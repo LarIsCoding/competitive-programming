533e53816997a06bc4e54ad0964b5fc5f185905c
Larissa Gomes de Stefano Escaliante <ra219788@oliveira.lab.ic.unicamp.br>
2022-11-04 14:44:38 -0300

    wip

---
+++ aulas-maratonic/larissa/aula-2/churrasc.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   // Otimização de input
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+
+   int n, k, aux;
+   cin >> n >> k;
+
+   priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > notas;
+   queue<pair<>>
+
+   for(int i = 0; i < k; i++) {
+       cin >> aux;
+       notas.push(aux);
+   }
+
+   return 0;
+}
