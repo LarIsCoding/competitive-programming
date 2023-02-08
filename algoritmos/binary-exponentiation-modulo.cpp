c703a8b546fb4e7fdf5f517f3c6cf1d1e9928ca9
Yasmin Kaline <63754896+Ka1ine@users.noreply.github.com>
2023-02-08 02:13:26 -0300

    feat: exponenciação rápida com módulo

---
+++ algoritmos/binary-exponentiation-modulo.cpp
@@ -0,0 +1,10 @@
+ll binpow(ll a, ll b, ll mod) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
