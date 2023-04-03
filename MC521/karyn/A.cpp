665efed1b184b1e62861f2e11e2760385c6fc536
coookie28k <123739912+coookie28k@users.noreply.github.com>
2023-04-03 01:30:40 -0300

    Create A.cpp

---
+++ MC521/karyn/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    int angulo;
+    while(cin >> angulo){
+    if (angulo%6 == 0) cout << 'Y' << endl;
+    else cout << 'N' << endl; }
+    return 0;
+}
