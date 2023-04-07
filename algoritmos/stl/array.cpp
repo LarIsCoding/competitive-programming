62a7355f62649efb08319bdf777ea4e17ef85643
LarissaEscaliante <larissastefano12@gmail.com>
2023-04-07 20:58:44 -0300

    array and vactor operations

---
+++ algoritmos/stl/array.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+
+using namespace std;
+
+typedef struct {
+    int id;
+    int solved;
+    int penalty;
+} team;
+
+bool icpc_cmp(team a, team b) {
+    if (a.solved != b.solved)
+        return a.solved > b.solved;
+    else if (a.penalty != b.penalty)
+        return a.penalty < b.penalty;
+    else
+        return a.id < b.id;
+}
+
+int main() {
+
+    team nus[4] = {
+        { 1, 1, 10},
+        {2, 3, 60},
+        {3, 1, 20},
+        {4, 3, 60}
+    };
+
+    sort(nus, nus + 4, icpc_cmp);
+
+    int arr[] = {10, 7, 2, 15, 4};
+    sort(arr, arr + 5); 
+    next_permutation(arr, arr + 5); // 2, 4, 7, 10, 15 −> 2, 4, 7, 15, 10
+    next_permutation(arr, arr + 5); // 2, 4, 7, 15, 10 −> 2, 4, 10, 7, 15
+
+    return 0;
+}
