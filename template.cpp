1dc4a4e186cb83406b8e4e97c3d96a7be2019dff
LarissaEscaliante <larissastefano12@gmail.com>
2023-04-11 10:44:24 -0300

    fix template

--- template.cpp
+++ template.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-   // Otimização de input
-   ios::sync_with_stdio(false);
-   cin.tie(0);
+    // Otimização de input
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-   return 0;
+    return 0;
 }
