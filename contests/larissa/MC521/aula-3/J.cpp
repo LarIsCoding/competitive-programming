6990e7d8a75e96bc0b2e1eeafa992fa12491652e
LarissaEscaliante <larissastefano12@gmail.com>
2023-04-19 09:25:12 -0300

    J.cpp

--- contests/larissa/MC521/aula-3/J.cpp
+++ contests/larissa/MC521/aula-3/J.cpp
@@ -14,52 +14,55 @@ int main() {
     cin.tie(0);
 
     int n, m;
-    cin >> n >> m;
 
-    vi conexoes;
-    conexoes.assign(26, 0);
+    while(cin >> n) {
+        cin >> m;
 
-    queue<ii> todo;
-    vector<vi> adj;
-    adj.assign(26, vi());
+        vi conexoes;
+        conexoes.assign(26, 0);
 
-    for(int i = 0; i < 3; i++) {
-        char v;
-        cin >> v;
-        v -= 'A';
-        conexoes[v] = 3;
-        todo.push(make_pair(v, 0));
-    }
+        queue<ii> todo;
+        vector<vi> adj;
+        adj.assign(26, vi());
 
-    for(int i = 0; i < m; i++) {
-        char v1, v2;
-        cin >> v1 >> v2;
-        v1 -= 'A';
-        v2 -= 'A';
-        adj[v1].push_back(v2);
-        adj[v2].push_back(v1);
-    }
+        for(int i = 0; i < 3; i++) {
+            char v;
+            cin >> v;
+            v -= 'A';
+            conexoes[v] = 3;
+            todo.push(make_pair(v, 0));
+        }
 
-    int falta = n - 3;
-    int years = 0;
-    while(!todo.empty() && falta > 0) {
-        int v = todo.front().first;
-        int year = todo.front().second;
-        for(vi::iterator it = adj[v].begin(); it != adj[v].end(); it++) {
-            conexoes[*it]++;
-            if(conexoes[*it] == 3) {
-                years = year + 1;
-                todo.push(make_pair(*it, years));
-                falta--;
-            }
-        } 
-        todo.pop();
-    }
+        for(int i = 0; i < m; i++) {
+            char v1, v2;
+            cin >> v1 >> v2;
+            v1 -= 'A';
+            v2 -= 'A';
+            adj[v1].push_back(v2);
+            adj[v2].push_back(v1);
+        }
 
-    if(falta == 0) {
-        cout << "WAKE UP IN, " << years << ", YEARS\n";
-    } else {
-        cout << "THIS BRAIN NEVER WAKES UP\n";
+        int falta = n - 3;
+        int years = 0;
+        while(!todo.empty() && falta > 0) {
+            int v = todo.front().first;
+            int year = todo.front().second;
+            for(vi::iterator it = adj[v].begin(); it != adj[v].end(); it++) {
+                conexoes[*it]++;
+                if(conexoes[*it] == 3) {
+                    years = year + 1;
+                    todo.push(make_pair(*it, years));
+                    falta--;
+                }
+            } 
+            todo.pop();
+        }
+
+        if(falta == 0) {
+            cout << "WAKE UP IN, " << years << ", YEARS\n";
+        } else {
+            cout << "THIS BRAIN NEVER WAKES UP\n";
+        }
     }
 
     return 0;
