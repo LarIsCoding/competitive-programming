75a8d7449d1b7bed8f149636615b6a9f5c51b0ca
LarissaEscaliante <larissastefano12@gmail.com>
2023-08-02 16:09:17 -0300

    mdc

---
+++ contests/larissa/planilha-obi/bash-and-a-tough-math-puzzle.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int,bool> ib;
+typedef vector<int> vi;
+typedef long long int ll;
+
+#define mp(i, j) make_pair(i, j)
+#define f first
+#define s second
+
+const int inf = (int)1e9;
+const ll mod = (ll)1e9 + 7;
+const ll linf = (ll)1e17;
+
+class SegmentTree {
+
+    private:
+    vector<ib> st; vi A; // st -> indice do menor valor; A -> vetor com valores
+    int n;
+
+    int left(int p) {
+        return p << 1;
+    }
+
+    int right(int p) {
+        return (p << 1) + 1;
+    }
+
+    void build(int p, int L, int R) {
+        if(L == R) {
+            st[p] = mp(A[L]);
+            return;
+        }
+
+        build(left(p), L, (L + R)/2);
+        build(right(p), (L + R)/2 + 1, R);
+
+        int p1 = st[left(p)];
+        int p2 = st[right(p)];
+
+        st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+    }
+
+    int rmq(int p, int L, int R, int i, int j) {
+        if(i > R || j < L) return -1;
+        if(L >= i && R <= j) return st[p];
+
+        int p1 = rmq(left(p), L, (L + R)/2, i, j);
+        int p2 = rmq(right(p), (L + R)/2 + 1, R, i, j);
+
+        if(p1 == -1) return p2;
+        if(p2 == -1) return p1;
+        return (A[p1] <= A[p2]) ? p1 : p2;
+    }
+
+    void update(int p, int L, int R, int i, int v) {
+        if(i > R || i < L) return;
+
+        if(i == L && i == R) {
+            st[p] = L;
+            A[L] = v;
+            return;
+        }
+
+        update(left(p), L, (L + R)/2, i, v);
+        update(right(p), (L + R)/2 + 1, R, i, v);
+
+        int p1 = st[left(p)];
+        int p2 = st[right(p)];
+
+        st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+    }
+
+    public:
+    SegmentTree(const vi &_A) {
+        A = _A;
+        n = (int)A.size();
+        st.assign(4 * n, 0);
+        build(1, 0, n - 1);
+    }
+
+    void update(int i, int v) {
+        update(1, 0, n - 1, i, v);
+    }
+
+    int rmq(int i, int j) {
+        return rmq(1, 0, n - 1, i, j);
+    }
+};
+
+vi v;
+
+int main() {
+    // Otimização de input
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++) {
+        cin>>v[i];
+    }
+
+    SegmentTree st(v);
+
+    int q;cin>>q;
+
+    while(q--) {
+        int op;cin>>op;
+        if(op==1) {
+            int l,r,x;cin>>l>>r>>x;l--;r--;
+            string r = st.rmq(l,r,x) ? "YES\n" : "NO\n";
+            cout<<r;
+        } else {
+            int i,y;cin>>i>>y;i--;
+            st.update(i,y);
+        }
+    }
+
+    return 0;
+}
