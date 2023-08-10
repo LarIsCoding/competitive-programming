3b0b869b15af5ef31e6a00265850a2daab806275
LarissaEscaliante <larissastefano12@gmail.com>
2023-08-10 15:13:26 -0300

    graph min path algorithms

---
+++ algoritmos/graphs/djikstra.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+
+using namespace std;
+
+const int INF = 1000000000;
+vector<vector<pair<int, int> > > adj;
+
+void dijkstra(int s, vector<int> &d, vector<int> &p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+    vector<bool> u(n, false);
+
+    d[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+        for (int j = 0; j < n; j++) {
+            if (!u[j] && (v == -1 || d[j] < d[v]))
+                v = j;
+        }
+
+        if (d[v] == INF)
+            break;
+
+        u[v] = true;
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+            }
+        }
+    }
+}
+
+vector<int> restore_path(int s, int t, vector<int> const& p) {
+    vector<int> path;
+
+    for (int v = t; v != s; v = p[v])
+        path.push_back(v);
+    path.push_back(s);
+
+    reverse(path.begin(), path.end());
+    return path;
+}
