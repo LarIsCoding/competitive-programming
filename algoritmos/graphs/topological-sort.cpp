2074cdbd3738cccdef7e3749f1841108d111721e
LarissaEscaliante <larissastefano12@gmail.com>
2023-08-11 00:29:59 -0300

    new algorithms

--- algoritmos/graphs/topological-sort.cpp
+++ algoritmos/graphs/topological-sort.cpp
@@ -2,25 +2,41 @@
 
 using namespace std;
 
-#define UNVISITED 2
-#define VISITED 1
-
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef vector<int> vi;
 
+#define UNVISITED 2
+#define PROCESSING 3
+#define VISITED 1
+
 vi dfs_num;
-vector<vii> AdjList;
+vector<vii> adj;
 vi topoSort;
 
 void dfs(int u) {
     dfs_num[u] = VISITED;
-    for(int j = 0; j < (int) AdjList[u].size(); j++) {
-        ii v = AdjList[u][j];
+    for(int j = 0; j < (int) adj[u].size(); j++) {
+        ii v = adj[u][j];
         if(dfs_num[v.first] == UNVISITED)
             dfs(v.first);
     } 
     topoSort.push_back(u);
+}
+
+// checa se é possivel
+bool dfs2(int u) {
+    dfs_num[u] = PROCESSING;
+    for(int j = 0; j < (int) adj[u].size(); j++) {
+        int v = adj[u][j];
+        if(dfs_num[v] == UNVISITED)
+            dfs(v);
+        if(dfs_num[v] == PROCESSING)
+            return false;
+    } 
+    dfs_num[u] = VISITED;
+    topoSort.push_back(u);
+    return true;
 }
 
 int main() {
