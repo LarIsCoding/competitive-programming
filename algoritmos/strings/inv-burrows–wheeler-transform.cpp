23a36edf26e13bfd56eee37e13fbc4bfd424c41c
LarIsCoding <51219842+LarIsCoding@users.noreply.github.com>
2023-10-18 10:20:28 -0300

    Update inv-burrows–wheeler-transform.cpp

--- algoritmos/strings/inv-burrows–wheeler-transform.cpp
+++ algoritmos/strings/inv-burrows–wheeler-transform.cpp
@@ -1,3 +1,8 @@
+// Invert Burrows Wheeler
+//
+// BWT pega uma string s e cria uma string t onde ti é o último caracter da i-ésima menor rotação da string s.
+// InvertBWT pega a string t e transforma em s
+
 #include <bits/stdc++.h>
 
 using namespace std;
