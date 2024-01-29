2c9766cf82986b23e3385e530a9991641de76859
Larissa Gomes de Stefano Escaliante <ra219788@panama.lab.ic.unicamp.br>
2024-01-29 17:59:43 -0300

    k

---
+++ study/summer-school/day-6/k.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef long long int ll;
+
+#define mp(i, j) make_pair(i, j)
+#define f first
+#define s second
+
+const int inf = (int)1e9;
+const ll mod = (ll)1e9 + 7;
+const ll linf = (ll)1e17;
+#define EPS 1e-9
+
+void dbg_out() {cerr << endl; }
+template <typename H, typename ...T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) {cerr << #__VA_ARGS__ << ':' ; dbg_out(__VA_ARGS__); } 
+
+vector<pair<int,bool>> events;
+
+void solve() {
+    int n,k;cin>>n>>k;
+
+    for(int i=0;i<n;i++) {
+        int l,r;cin>>l>>r;
+        events.push_back({l,false});
+        events.push_back({r,true});
+    }
+
+    sort(events.begin(),events.end());
+
+    int acc=0;
+    for(auto i:events) {
+        if()
+    }
+}
+
+int main() {
+    // Otimização de input
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t=1;
+    //cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
